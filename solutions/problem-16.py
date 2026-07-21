19eebb266cf43ff1dd333887e160e7ab1f6f238e
amootasa <amootasaleh@gmail.com>
2026-07-21 21:50:02 +0300

    Add multiplication table and factorial calculation scripts

---
+++ solutions/problem-16.py
@@ -0,0 +1,9 @@
+num = int (input("Enter any integer number: "))
+print("Multiplication table for the number : ",num )
+
+i= 1
+while i <=10:
+    result =num * i
+    print(num ," x ",i ," = ", result)
+    i+=1
+print("Done!!")
---
+++ solutions/problem-2.py
@@ -0,0 +1,8 @@
+
+num = int (input("Enter any integer number: "))
+if num < 0:
+    print("Sorry, not integer number!! Try again!")
+while num >=0:
+    print (num)
+    num -= 1
+    
